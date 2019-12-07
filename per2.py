32f73297cc6e3d5c516e4293b6488f3907adbb82
Ali2068 <ali.arrasyid@students.amikom.ac.id>
2019-12-07 17:06:23 +0700

    Kalkulator Python Sederhana

---
+++ per2.py
@@ -0,0 +1,18 @@
+angka1 = int(input('Masukkan Angka Pertama : '))
+angka2 = int(input('Masukkan Angka Kedua : '))
+print('Operator : \n1.Penjumlahan \n2.Pengurangan \n3.Perkalian \n4.Pembagian')
+pilih = int(input('Masukkan Operator : '))
+if pilih == 1:
+    tambah = angka1+angka2
+    print('Hasilnya adalah = ',tambah)
+elif pilih == 2:
+    kurang = angka1-angka2
+    print('Hasilnya adalah = ',kurang)
+elif pilih == 3:
+    kali = angka1*angka2
+    print('Hasilnya adalah = ',kali)
+elif pilih == 4:
+    bagi = angka1/angka2
+    print('Hasilnya adalah = ',bagi)
+else:
+    print('Operator yang anda masukkan tidak ada')
